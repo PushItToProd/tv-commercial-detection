97d71b5681466da421f541c07e6da3d87f088dd6
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-03-11 13:45:38 -0700

    tweak check_classification.sh

--- server/check_classification.sh
+++ server/check_classification.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+num_images=0
+num_incorrect=0
+
+incorrectly_marked_as_ads=()
+incorrectly_marked_as_content=()
+
 for f in frames/*.png; do
+    ((num_images++))
     echo "Processing $f"
     classification=$(python classify.py "$f")
 
@@ -15,5 +22,17 @@ for f in frames/*.png; do
         echo "$f - correct"
     else
         echo "$f - incorrect (classified as $classification, expected $actual)"
+        ((num_incorrect++))
+        if [[ "$actual" == "ad" ]]; then
+            incorrectly_marked_as_ads+=("$f")
+        else
+            incorrectly_marked_as_content+=("$f")
+        fi
     fi
 done
+
+echo "Processed $num_images images, $num_incorrect incorrect classifications."
+echo "Num. incorrectly marked as ads: ${#incorrectly_marked_as_ads[@]}"
+echo "  Incorrectly marked as ads: ${incorrectly_marked_as_ads[*]}"
+echo "Num. incorrectly marked as content: ${#incorrectly_marked_as_content[@]}"
+echo "  Incorrectly marked as content: ${incorrectly_marked_as_content[*]}"
